6d3253bcade5e0169fd2d444cc63ee2124e2e4fa
lucaschang95 <xingzhao.chang@gmail.com>
2020-03-09 14:23:41 +0800

    optimize pic loading

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-const navbar = document.querySelector('#navbar');
-const navHome = document.querySelector('#navbar li:nth-of-type(1)');
-const navAbout = document.querySelector('#navbar li:nth-of-type(2)');
-const navGallery = document.querySelector('#navbar li:nth-of-type(3)');
-const navContact = document.querySelector('#navbar li:nth-of-type(4)');
+const navbar = document.querySelector('.navbar');
+const navHome = document.querySelector('.navbar li:nth-of-type(1)');
+const navAbout = document.querySelector('.navbar li:nth-of-type(2)');
+const navGallery = document.querySelector('.navbar li:nth-of-type(3)');
+const navContact = document.querySelector('.navbar li:nth-of-type(4)');
 const homePositionDot = document.querySelector('#home .locationDot');
 const homeMap = document.querySelector('#home iframe');
 const downArrow = document.querySelector('#home img');
@@ -13,8 +13,6 @@ const aboutMap = document.querySelector('#about iframe');
 function scrollToAbout() {
     document.querySelector('#about').scrollIntoView();
 }
-
-downArrow.addEventListener('click', scrollToAbout);
 
 navHome.addEventListener('click', function() {
     console.log('click navHome!');
@@ -31,29 +29,19 @@ navContact.addEventListener('click', function() {
     document.querySelector('#contact').scrollIntoView();
 });
 
-homePositionDot.addEventListener('mouseenter', function(){
-    console.log('mouse enter!');
-    homeMap.style.visibility = 'visible';
-});
+// homePositionDot.addEventListener('mouseenter', function(){
+//     console.log('mouse enter!');
+//     homeMap.style.visibility = 'visible';
+// });
 
-homePositionDot.addEventListener('mouseleave', function(){
-    console.log('mouse leave!');
-    homeMap.style.visibility = 'hidden';
-});
-
-/*
-aboutPositionDot.addEventListener('mouseenter', function(){
-    console.log('mouse enter!');
-    aboutMap.style.visibility = 'visible';
-});
+// homePositionDot.addEventListener('mouseleave', function(){
+//     console.log('mouse leave!');
+//     homeMap.style.visibility = 'hidden';
+// });
 
 
-aboutPositionDot.addEventListener('mouseleave', function(){
-    console.log('mouse leave!');
-    aboutMap.style.visibility = 'hidden';
-});
-*/
-
-window.addEventListener('scroll', function() {
-
+window.addEventListener('load', function() {
+    console.log('page is fully loaded!');
+    document.querySelector('section#home h2').classList.add('welcomeAnimationQuick');
+    document.querySelector('section#home a').classList.add('welcomeAnimationSlow');
 });
